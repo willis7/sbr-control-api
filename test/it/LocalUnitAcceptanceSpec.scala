63fc76e89c79629cd30e6520a5f9a7a18af89ada
Nigel Perkins <nigelhp@yahoo.co.uk>
2018-03-23 13:09:48 +0000

    Add initial acceptance test for latest local unit retrieval by ern and lurn

---
+++ test/it/LocalUnitAcceptanceSpec.scala
@@ -0,0 +1,96 @@
+import fixture.{ ServerAcceptanceSpec, WithWireMockHBase }
+import org.scalatest.OptionValues
+import play.api.http.Status.OK
+import play.api.libs.json.Json
+import play.mvc.Http.MimeTypes.JSON
+
+class LocalUnitAcceptanceSpec extends ServerAcceptanceSpec with WithWireMockHBase with OptionValues {
+  private val ERN = "1000000012"
+  private val LURN = "900000011"
+  private val EarliestPeriod = "201802"
+  private val LatestPeriod = "201803"
+
+  // TODO move all this as we develop an implementation ...
+  private case class Address(line1: String, line2: String, line3: String, line4: String, line5: String, postcode: String)
+  private case class EnterpriseLink(ern: String, entref: String)
+  private case class LocalUnit(lurn: String, luref: String, name: String, tradingStyle: String, sic07: String,
+    employees: Int, enterprise: EnterpriseLink, address: Address)
+
+  private object Address {
+    implicit val reads = Json.reads[Address]
+  }
+
+  private object EnterpriseLink {
+    implicit val reads = Json.reads[EnterpriseLink]
+  }
+
+  private object LocalUnit {
+    implicit val reads = Json.reads[LocalUnit]
+  }
+  // end TO MOVE
+
+  private val LocalUnitMultiplePeriodsHBaseResponseBody =
+    s"""{"Row": ${
+      List(
+        aRowWith(
+          key = s"${ERN.reverse}~$EarliestPeriod~$LURN",
+          columns = aColumnWith(name = "lurn", value = LURN),
+          aColumnWith(name = "luref", value = "some-luref"),
+          aColumnWith(name = "ern", value = ERN),
+          aColumnWith(name = "entref", value = "some-entref"),
+          aColumnWith(name = "name", value = s"name-$EarliestPeriod"),
+          aColumnWith(name = "tradingstyle", value = s"tradingstyle-$EarliestPeriod"),
+          aColumnWith(name = "address1", value = s"address1-$EarliestPeriod"),
+          aColumnWith(name = "address2", value = s"address2-$EarliestPeriod"),
+          aColumnWith(name = "address3", value = s"address3-$EarliestPeriod"),
+          aColumnWith(name = "address4", value = s"address4-$EarliestPeriod"),
+          aColumnWith(name = "address5", value = s"address5-$EarliestPeriod"),
+          aColumnWith(name = "postcode", value = s"postcode-$EarliestPeriod"),
+          aColumnWith(name = "sic07", value = s"sic07-$EarliestPeriod"),
+          aColumnWith(name = "employees", value = "99")
+        ),
+        aRowWith(
+          key = s"${ERN.reverse}~$LatestPeriod~$LURN",
+          columns = aColumnWith(name = "lurn", value = LURN),
+          aColumnWith(name = "luref", value = "some-luref"),
+          aColumnWith(name = "ern", value = ERN),
+          aColumnWith(name = "entref", value = "some-entref"),
+          aColumnWith(name = "name", value = s"name-$LatestPeriod"),
+          aColumnWith(name = "tradingstyle", value = s"tradingstyle-$LatestPeriod"),
+          aColumnWith(name = "address1", value = s"address1-$LatestPeriod"),
+          aColumnWith(name = "address2", value = s"address2-$LatestPeriod"),
+          aColumnWith(name = "address3", value = s"address3-$LatestPeriod"),
+          aColumnWith(name = "address4", value = s"address4-$LatestPeriod"),
+          aColumnWith(name = "address5", value = s"address5-$LatestPeriod"),
+          aColumnWith(name = "postcode", value = s"postcode-$LatestPeriod"),
+          aColumnWith(name = "sic07", value = s"sic07-$LatestPeriod"),
+          aColumnWith(name = "employees", value = "100")
+        )
+      ).mkString("[", ",", "]")
+    }
+    }"""
+
+  feature("retrieve the latest local unit") {
+    // TOD change "ignore" to "scenario" to make this runnable
+    ignore("for an Enterprise reference (ERN) and Local Unit reference (LURN) when the local unit exists over many periods") { wsClient =>
+      Given(s"a local unit exists for multiple periods in HBase with a Local Unit reference of $LURN and Enterprise reference of $ERN")
+      stubHbaseFor(aLocalUnitRequest(withErn = ERN, withLurn = LURN).willReturn(
+        anOkResponse().withBody(LocalUnitMultiplePeriodsHBaseResponseBody)
+      ))
+
+      When(s"the latest unit with Local Unit reference $LURN and Enterprise reference $ERN is requested")
+      val response = await(wsClient.url(s"/v1/enterprises/$ERN/localunits/$LURN?max=1").get())
+
+      Then(s"the details of the local unit with Local Unit reference $LURN for the latest period are returned")
+      response.status shouldBe OK
+      response.header("Content-Type").value shouldBe JSON
+      response.json.as[LocalUnit] shouldBe
+        LocalUnit(lurn = LURN, luref = "some-luref", name = s"name-$LatestPeriod",
+          tradingStyle = s"tradingstyle-$LatestPeriod", sic07 = s"sic07-$LatestPeriod", employees = 100,
+          enterprise = EnterpriseLink(ern = ERN, entref = "some-entref"),
+          address = Address(line1 = s"address1-$LatestPeriod", line2 = s"address2-$LatestPeriod",
+            line3 = s"address3-$LatestPeriod", line4 = s"address4-$LatestPeriod", line5 = s"address5-$LatestPeriod",
+            postcode = s"postcode-$LatestPeriod"))
+    }
+  }
+}
--- test/it/fixture/WithWireMockHBase.scala
+++ test/it/fixture/WithWireMockHBase.scala
@@ -13,8 +13,13 @@ trait WithWireMockHBase extends WithWireMock { this: Suite =>
   override val wireMockPort = 8075
 
   def anEnterpriseRequest(withId: String): MappingBuilder =
-    get(urlEqualTo(s"/sbr_control_db:enterprise/${withId.reverse}~*/d")).
-      withHeader("Accept", matching(JSON))
+    getHbaseJson(s"/sbr_control_db:enterprise/${withId.reverse}~*/d")
+
+  def aLocalUnitRequest(withErn: String, withLurn: String): MappingBuilder =
+    getHbaseJson(s"/sbr_control_db:local_unit/${withErn.reverse}~*~$withLurn/d")
+
+  private def getHbaseJson(url: String): MappingBuilder =
+    get(urlEqualTo(url)).withHeader("Accept", matching(JSON))
 
   def anOkResponse(): ResponseDefinitionBuilder =
     aResponse().withStatus(OK)
@@ -24,4 +29,15 @@ trait WithWireMockHBase extends WithWireMock { this: Suite =>
 
   val stubHbaseFor: MappingBuilder => Unit =
     WireMock.stubFor
+
+  def aRowWith(key: String, columns: String*): String =
+    s"""|{"key": "${hbaseEncode(key)}",
+        | "Cell": ${columns.mkString("[", ",", "]")}
+        |}""".stripMargin
+
+  def aColumnWith(name: String, value: String, timestamp: Long = 1520333985745L): String =
+    s"""|{"column": "${hbaseEncode(name)}",
+        | "timestamp": $timestamp,
+        | "$$": "${hbaseEncode(value)}"
+        |}""".stripMargin
 }
